077887b265b5bfe06984fe36cdd60ac1d5c91e1f
Doug Patti <douglas.patti@gmail.com>
2013-06-10 21:01:56 -0400

    Initial commit

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Mixtapes::Application.config.session_store :cookie_store, key: '_mixtapes_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Mixtapes::Application.config.session_store :active_record_store
