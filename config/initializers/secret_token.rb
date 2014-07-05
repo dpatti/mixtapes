a6b962fa71d9528e619d274cbd27e55f1df1e416
Doug Patti <douglas.patti@gmail.com>
2014-07-05 16:21:11 -0400

    use environment-specific secret token
    This was a prod vulnerability. Rookie mistake.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,8 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Mixtapes::Application.config.secret_token = '84e7e0ecc241843c17e1ead64d5fa0f0d38216cf0a3e23a436e8edde0e0fcd43537545b1afc229debc5cad619d488758a3948d678688bb9aa68dd7442386911d'
+Mixtapes::Application.config.secret_token = if Rails.env.development? or Rails.env.test?
+  '.' * 30
+else
+  Settings.secret_token
+end
