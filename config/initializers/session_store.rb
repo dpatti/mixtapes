ce24ec3dd9d01b2503778806eb564a71c9e46c43
Doug Patti <douglas.patti@gmail.com>
2014-08-10 13:51:31 -0400

    persist cookie session for 6 months

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,5 @@
-# Be sure to restart your server when you modify this file.
-
-Mixtapes::Application.config.session_store :cookie_store, key: '_mixtapes_session'
-
-# Use the database for sessions instead of the cookie-based default,
-# which shouldn't be used to store highly confidential information
-# (create the session table with "rails generate session_migration")
-# Mixtapes::Application.config.session_store :active_record_store
+Mixtapes::Application.config.session_store :cookie_store, {
+  :key => '_mixtapes_session',
+  :path => '/',
+  :expire_after => 6.months,
+}
