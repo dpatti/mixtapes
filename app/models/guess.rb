5b00e2f6ba021ec2be6bb764ce89d4fd6f7a6b6d
Doug Patti <douglas.patti@gmail.com>
2017-09-04 12:07:00 -0400

    fix guesses calculation
    I forgot to discount one's own mix both when scoring and counting
    guesses for, so that's been a weird thing that's probably happened in
    the past. Basically people who filled in their own (even when the
    instructions told you that you didn't need to) got bonus points or
    something.

--- app/models/guess.rb
+++ app/models/guess.rb
@@ -6,4 +6,8 @@ class Guess < ActiveRecord::Base
   belongs_to :user, :foreign_key => :user_guessed_id
 
   default_scope -> { includes(:mixtape) }
+
+  def self.non_self
+    all.reject { |g| g.user_id == g.mixtape&.user&.id }
+  end
 end
--- script/calculate_guesses.rb
+++ script/calculate_guesses.rb
@@ -1,18 +1,21 @@
-User.all.each do |user| 
-  guesses = user.guesses.includes(:mixtape).all 
-  next if guesses.size == 0 
- 
-  score = guesses.map do |g| 
-    g.user_guessed_id == g.mixtape.user_id ? 1 : 0 
-  end.reduce(:+) 
- 
-  guesses_for = -1 
-  if user.mixtape 
-    guesses_for = Guess.where(:mixtape_id => user.mixtape.id).map do |g| 
-      g.user_guessed_id == user.id ? 1 : 0 
-    end.reduce(:+) 
-  end 
- 
-  puts "User: #{ user.name } - Score: #{ score } - Guesses for: #{ guesses_for }" 
-end 
+User.all.each do |user|
+  guesses = user.guesses.includes(:mixtape).non_self
 
+  score = -1
+  if guesses.length > 0
+    score = guesses.map do |g|
+      g.user_guessed_id == g.mixtape.user_id ? 1 : 0
+    end.reduce(0, :+)
+  end
+
+  guesses_for = -1
+  if user.mixtape
+    guesses_for = Guess.where(:mixtape_id => user.mixtape.id).non_self.map do |g|
+      g.user_guessed_id == user.id ? 1 : 0
+    end.reduce(0, :+)
+  end
+
+  if score > 0 || guesses_for >= 0
+    puts "User: #{ user.name } - Score: #{ score } - Guesses for: #{ guesses_for }"
+  end
+end
