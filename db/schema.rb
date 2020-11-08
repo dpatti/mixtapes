96c9037dc02387df9d03f81713aa398e681f2f27
Doug Patti <douglas.patti@gmail.com>
2020-11-08 14:01:28 -0500

    add comment edited_at column

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20201101205625) do
+ActiveRecord::Schema.define(version: 20201108185637) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "user_id"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20201101205625) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "deleted",    default: false
+    t.datetime "edited_at"
   end
 
   create_table "contests", force: :cascade do |t|
